f8a300620033448495af3172c47c6fe5ba2a7a35
ditzzzXZ <ditzzz.ofc@gmail.com>
2026-01-07 17:57:34 +0700

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -3,25 +3,26 @@ import { NextResponse } from 'next/server';
 export function middleware(request) {
   const { pathname } = request.nextUrl;
   
-  // Define supported locales
+  // This helps avoid an infinite redirect loop
+  if (
+    pathname.startsWith('/_next') || 
+    pathname.includes('/api/') || 
+    pathname.includes('.')
+  ) {
+    return;
+  }
+
   const locales = ['en', 'id', 'ms', 'ja', 'zh', 'es', 'es-MX'];
-  
-  // Check if the URL already has a locale
-  const pathnameHasLocale = locales.some(
-    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
+  const pathnameIsMissingLocale = locales.every(
+    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
   );
 
-  if (pathnameHasLocale) return;
-
-  // Redirect if there is no locale
-  const locale = 'en'; // Default to English for now
-  request.nextUrl.pathname = `/${locale}${pathname}`;
-  return NextResponse.redirect(request.nextUrl);
+  // If there's no language in the URL, redirect to /en
+  if (pathnameIsMissingLocale) {
+    return NextResponse.redirect(new URL(`/en${pathname}`, request.url));
+  }
 }
 
 export const config = {
-  matcher: [
-    // Skip all internal paths (_next, api, etc)
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
-  ],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };
